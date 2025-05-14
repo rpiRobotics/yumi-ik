6eaedc15035db6ccb0aaff137541bc396245af70
Alex Elias <aelias36@yahoo.com>
2025-05-14 12:38:41 -0400

    Update test_SEW_jacobian.m

--- unit_tests/test_SEW_jacobian.m
+++ unit_tests/test_SEW_jacobian.m
@@ -10,14 +10,14 @@ e_r = rand_normal_vec;
 % e_r = [0;0;1];
 q = rand_angle([7 1]);
 
-SEW = yumi.sew_abb(e_r);
+SEW = yumi.sew_conv_h4(e_r);
 
 delta = 1e-6;
 direction = rand_angle([7 1]); direction = direction / norm(direction);
 delta_vec = delta*direction;
 
-diff_numeric = (SEW.fwd_kin(kin, q+delta_vec/2) - SEW.fwd_kin(kin, q-delta_vec/2)) / delta;
-diff_analytical = SEW.jacobian(kin, q) * direction;
+diff_numeric = (SEW.fwd_kin_q(q+delta_vec/2, kin) - SEW.fwd_kin_q(q-delta_vec/2, kin)) / delta;
+diff_analytical = SEW.full_J_psi(q, kin) * direction;
 err_list(i) = diff_numeric - diff_analytical;
 end
 
